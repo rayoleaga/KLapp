41368007f338ae26f61bc661cd7108da6fa234a2
Ray Oleaga <roleaga@Rays-MBP.home>
2016-03-17 19:44:45 -0400

    added config

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
-	'port': process.env.PORT || 8080,
-	'database': 'mongodb://localhost:27017/kldb',
-	'secret': 'klrhodeislandsingeranddiva'
+	'port': process.env.port || 8080,
+	'database': 'mongodb://rayoleaga:quecosas13@ds057254.mongolab.com:57254/yourdesignbuddy',
+	'secret': 'rayapiquecosas13'
 };
+
+
